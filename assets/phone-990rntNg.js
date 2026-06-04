dcbee100fc3021da1d093a2037dbdd801c14cd99
SachaTy <SachaTy>
2026-06-04 13:27:02 +0200

    Updates

--- assets/phone-990rntNg.js
+++ assets/phone-990rntNg.js
@@ -1,4 +1,4 @@
-import{i as h}from"./index-Yvqd5Oz1.js";/**
+import{j as h}from"./index-CpaVshV0.js";/**
  * @license lucide-vue-next v0.395.0 - ISC
  *
  * This source code is licensed under the ISC license.
